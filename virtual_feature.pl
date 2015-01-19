191662215012bee126c413bbaf9bf9953724dd2c
Jamie Cameron <jcameron@webmin.com>
2015-01-18 16:04:40 -0800

    Git binaries can be in a new location http://virtualmin.com/node/35642

--- virtual_feature.pl
+++ virtual_feature.pl
@@ -136,8 +136,10 @@ else {
 # Setup gitweb if possible
 &$virtual_server::first_print($text{'feat_gitweb'});
 local $git = &has_command("git") || "git";
-local $gitdir = -e "/usr/lib/git-core/git-rev-list" ? "/usr/lib/git-core" :
-		$git =~ /^(.*)\// ? $1 : "/usr/bin";
+local $gitdir =
+	-e "/usr/lib/git-core/git-rev-list" ? "/usr/lib/git-core" :
+	-e "/usr/libexec/git-core/git-rev-list" ? "/usr/libexec/git-core" :
+	$git =~ /^(.*)\// ? $1 : "/usr/bin";
 local $src = &find_gitweb();
 local $gitweb = "$phd/git/gitweb.cgi";
 &virtual_server::copy_source_dest_as_domain_user($d, $src, $gitweb);
